c0152775ec5f530d92f44e1e3cd9a8c85742a249
anshu1016 <arunshukla98710@gmail.com>
2024-07-15 12:25:52 +0530

    Basic Setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { Button } from '@/components/ui/button'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Auth from '@/pages/auth'
+import Profile from '@/pages/profile'
+import Chat from '@/pages/chat'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/auth" element={<Auth/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/chat" element={<Chat/>}/>
+        <Route path="*" element={<Navigate to="/auth"/>}/>
+      </Routes>
+      
+      <Button>Click Me!</Button>
+    </div>
+  )
+}
+
+export default App
