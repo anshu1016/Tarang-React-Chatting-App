2fec68d4c6af6bcf5caf0355ec7a70f40e4ecb57
anshu1016 <arunshukla98710@gmail.com>
2024-08-06 15:16:16 +0530

    Signup Function Modified

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,41 +1,38 @@
 import jwt from "jsonwebtoken";
 import User from "../models/UserModel.js";
-import { response } from "express";
 
-const maxAge = 3*24*60*60*1000
+const maxAge = 3 * 24 * 60 * 60 * 1000;
 
-const createToken = (email,userId) =>{
-    return jwt.sign({email,userId},process.env.JWT_KEY,{expiresIn:maxAge})
+const createToken = (email, userId) => {
+  return jwt.sign({ email, userId }, process.env.JWT_KEY, { expiresIn: maxAge });
 }
 
-export const Signup = async(req,res,next) =>{
-    try{
-        const {email,password} = req.body;
-        if(!email || !password){
-            return res.status(400).send("Email and Password is required");
-        }
-        const user = await User.create({email,password});
-        response.cookie("jwt",createToken(email,user.id),{
-            maxAge,
-            secure:true,
-            sameSite: "None"
-        })
-        return res.status(201).json({
-            user:{
-                id: user.id,
-                email: user.email,
-                // firstName: user.firstName,
-                // lastName: user.lastName,
-                // image: user.image,
-                // color: user.color,
-                profileSetup : user.profileSetup
-            }
-        })
+export const Signup = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).send("Email and Password are required");
+    }
+    console.log('Creating user...');
+    const user = await User.create({ email, password });
+    console.log('User created:', user);
+    const token = createToken(email, user.id);
+    console.log('Token created:', token);
+    res.cookie("jwt", token, {
+      maxAge,
+      secure: true,
+      sameSite: "None"
+    });
+    return res.status(201).json({
+      user: {
+        id: user.id,
+        email: user.email,
+        profileSetup: user.profileSetup
+      }
+    });
 
-    }
-    catch(err){
-        console.log({err});
-        return res.status(500).send("Internal Server Error")
-    }
-    finally{}
+  } catch (err) {
+    console.log('Error during signup:', err);
+    return res.status(500).send("Internal Server Error");
+  }
 }
